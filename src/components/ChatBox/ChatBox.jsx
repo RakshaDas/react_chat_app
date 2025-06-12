dee67a287e49aa0fb0a70ab3e9e2c006aecfc3a4
Raksha <draksha54@gmail.com>
2025-06-12 23:19:25 +0530

    Build send message in chatbox

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -13,8 +13,17 @@ const ChatBox = () => {
         </p>
         <img src={assets.help_icon} className="help" alt="" />
       </div>
-    </div>
+
+/*<div className="chat-input">
+        <input type="text" placeholder="Send a message" />
+        <input type="file" id="image/png, image/jpeg" hidden />
+        <label htmlFor="image">
+          <img src={assets.gallery.icon} alt="" />
+        </label>
+        <img src={assets.send_button} alt="" />
+      </div>
+    </div> */
   );
 };
 
-export default ChatBox;
+export default ChatBox
