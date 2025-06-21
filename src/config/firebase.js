d0a19812f973334d4b7842b89b9504844196550c
Raksha <draksha54@gmail.com>
2025-06-21 23:39:52 +0530

    added firebase in config

---
+++ src/config/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBgiP5nrvsM-fGkfofuMvUeNxVRdWd4o8Q",
+  authDomain: "react-chat-app-c03b2.firebaseapp.com",
+  projectId: "react-chat-app-c03b2",
+  storageBucket: "react-chat-app-c03b2.firebasestorage.app",
+  messagingSenderId: "304579777980",
+  appId: "1:304579777980:web:be72b14b142ad353dac7b9",
+  measurementId: "G-H4CMCP21P4",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ProfileUpdate.css";
 import assets from "../../assets/assets";
 const ProfileUpdate = () => {
+  const [image, setImage] = useState(false);
+
   return (
     <div className="profile">
       <div className="profile-container">
@@ -9,20 +11,28 @@ const ProfileUpdate = () => {
           <h3>Profile Details</h3>
           <label htmlFor="avatar">
             <input
+              onChange={(e) => setImage(e.target.files[0])}
               type="file"
               id="avatar"
               accept="
             .png, .jpg,.jpeg"
               hidden
             />
-            <img src={assets.avatar_icon} alt="" />
+            <img
+              src={image ? URL.createObjectURL(image) : assets.avatar_icon}
+              alt=""
+            />
             upload profile image
           </label>
           <input type="text" placeholder="Your name" required />
           <textarea placeholder="write profile bio" required></textarea>
           <button type="submit">save</button>
         </form>
-        <img className="profile-pic" src={assets.logo_icon} alt="" />
+        <img
+          className="profile-pic"
+          src={image ? URL.createObjectURL(image) : assets.logo_icon}
+          alt=""
+        />
       </div>
     </div>
   );
